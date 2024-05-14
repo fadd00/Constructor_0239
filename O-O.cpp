01c43712a7195bb3400d8768b4000bf1d868c5c1
fadd00 <andhikahutama9@gmail.com>
2024-05-14 15:31:51 +0700

    membuat constructor

--- O-O.cpp
+++ O-O.cpp
@@ -2,6 +2,32 @@
 #include<string>
 using namespace std;
 
+class mahasiswa{
+    public:
+    int nim;
+    string nama;
+
+    public:
+    mahasiswa(){
+        nim = 0;
+        nama = "";
+    };
+    mahasiswa(int inim){
+            nim = inim;
+        }
+    mahasiswa(string inama){
+            nama = inama;
+    };
+    mahasiswa(int inim, string inama){
+            nim = inim;
+            nama = inama;
+        };
+void cetak(){
+    cout << "NIM = " << nim << endl;
+    cout << "Nama = " << nama << endl;
+    cout << endl;
+};
+};
 int main(){
-    
+
 }
