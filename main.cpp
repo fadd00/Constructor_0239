1fb9a8a6972ffd2711d270128ee26e7864950aa1
fadd00 <andhikahutama9@gmail.com>
2024-05-14 14:47:51 +0700

    membuat output

--- main.cpp
+++ main.cpp
@@ -28,5 +28,14 @@ void mahasiswa::printAll(){
 }
 
 int main(){
-    
+    mahasiswa mhs1("Ilam Compas");
+    mahasiswa mhs2("hapis samba");
+    mahasiswa mhs3("uno 13");
+    mahasiswa mhs4("farhad 15");
+
+    mhs1.printAll();
+    mhs2.printAll();
+    mhs3.printAll();
+    mhs4.printAll();
+    return 0;
 }
