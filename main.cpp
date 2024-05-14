090c87a9e603d609958d3fd89bf35fd8bd2e01b2
fadd00 <andhikahutama9@gmail.com>
2024-05-14 14:33:53 +0700

    membuat constructor mahasiswa

--- main.cpp
+++ main.cpp
@@ -6,6 +6,13 @@ class mahasiswa{
         static int nim;
         int id;
         string nama;
+    
+    void setID();
+
+    void printAll();
+    mahasiswa(string pnama){
+        
+    }
 };
 
 int main(){
