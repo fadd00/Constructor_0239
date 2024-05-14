6c8f855e5944983876c245ce0d46ffc061bf1084
fadd00 <andhikahutama9@gmail.com>
2024-05-14 14:56:44 +0700

    memasukan member

--- 2.cpp
+++ 2.cpp
@@ -4,6 +4,14 @@ using namespace std;
 
 class mahasiswa{
     private:
+    static int nim;
+
+    public:
+    int id;
+    string nama;
+
+    void setID();
+    void printAll();
 };
 
 int main(){
