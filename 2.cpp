8212cae0d624b264f3778faa11f0ad656384cbad
fadd00 <andhikahutama9@gmail.com>
2024-05-14 15:06:27 +0700

    membuat prosedur mahasiswa printall

--- 2.cpp
+++ 2.cpp
@@ -22,7 +22,12 @@ class mahasiswa{
 };
 int mahasiswa::nim = 239;
 void mahasiswa::setID(){
-    
+    id = ++nim;
+}
+void mahasiswa::printAll() {
+    cout << "ID : " << id << endl;
+    cout << "Nama : " << nama << endl;
+    cout << endl;
 }
 int main(){
 
